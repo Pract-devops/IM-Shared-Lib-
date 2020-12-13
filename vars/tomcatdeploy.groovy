0a14028db7214a1944a53a552dec4dfbab35c230
Pract-devops <75196330+Pract-devops@users.noreply.github.com>
2020-12-13 16:27:13 +0530

    Create tomcatdeploy.groovy

---
+++ vars/tomcatdeploy.groovy
@@ -0,0 +1,11 @@
+de call(tomcatip,tomcatusr,finalWarName ){
+ sh "mv target/myweb*.war target/${finalWarName}.war"
+                sshagent(credentials: ['slave-one'], ignoreMissing: true) {
+                         // some block
+                sh "scp -o StrictHostKeyChecking=no target/${finalWarName}.war ${tomcatusr}@${tomcatip}:/opt/tomcat8/webapps"
+                sh """
+                        ssh ${tomcatusr}@${tomcatip} /opt/tomcat8/bin/shutdown.sh
+                        ssh ${tomcatusr}@${tomcatip} /opt/tomcat8/bin/startup.sh
+                    """
+                    }
+}
